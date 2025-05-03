4c303d6adfafd97fc26c32255762fe6fa9cf114d
celian <nik.sir2018@yandex.ru>
2025-05-03 16:41:21 +0300

    test again

---
+++ task_14.py
@@ -0,0 +1,23 @@
+class EvenNumbers:
+    def __init__(self, n: int):
+        self.cur = 0
+        self.count = 0
+        self.stop = n
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count >= self.stop:
+            raise StopIteration
+
+        result = self.cur
+        self.cur += 2
+        self.count += 1
+        return result
+
+
+evens = EvenNumbers(5)
+
+for i in evens:
+    print(i)
